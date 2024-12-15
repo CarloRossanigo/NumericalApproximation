940296b2dd7b6a507b474ba49da5cb7e63ccea24
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2024-12-15 14:59:21 +0100

    Update CicloLimite.m

--- FitzHugh-Nagumo/Matlab/CicloLimite.m
+++ FitzHugh-Nagumo/Matlab/CicloLimite.m
@@ -1,25 +1,34 @@
-k = load('EE_5000');
+k = load('RK4_5000');
 x = k(:,1); % tempo
 y = k(:,2); % V
 z = k(:,3); % W
 
 %%Se i passi sono 1000 toll a 10-3, altrimenti a 10-4 (entrambe)
-toll = 1e-4; % toll
-toll2=1e-3; % toll 2
+toll=1e-3; % toll 2
 %Trovo (100) indici per cui la |min(y)-y|<soglia
 tempo=find(abs(min(y)-y)<toll,100);
 inizio=x(tempo(1));
-
 fprintf('Tempo di inizio del ciclo limite: %.6f\n',inizio);
 %
-inizio_ciclo=y(tempo(1)+1);
-fine_ciclo = find(abs(y(tempo(1)+2:end) - inizio_ciclo) < toll,2) + tempo(1);
-    % Calcolo della periodicità del ciclo limite
-   %5 Per EE 1000 passi prendi fine_ciclo(1)
-    repeat_indice=fine_ciclo(2);
-    periodo=x(repeat_indice) - inizio;
-        fprintf('Periodicità del ciclo limite: %.6f\n',periodo);
+n = size(tempo, 1);  % Numero di indici trovati
+Indici = zeros(n, 1);  % Vettore per i nuovi indici
+j = 1;  % Indice per il vettore Indici
+Indici(j) = tempo(1);  % Aggiungo il primo indice
 
+% Ciclo per selezionare solo indici non consecutivi
+for i = 2:n
+    % Se la differenza tra il valore corrente e il precedente è maggiore di una soglia
+    if tempo(i) > tempo(i-1) + 1  % Aggiungo solo se la differenza è maggiore di 1
+        j = j + 1;  % Incrementa l'indice
+        Indici(j) = tempo(i);  % Aggiungo l'indice corrente
+    end
+end
+
+% Riduco la dimensione di Indici 
+Indici = Indici(1:j);  % Mantieni solo i valori validi
+Periodo=x(Indici(2))-x(Indici(1));
+
+        fprintf('Periodicità del ciclo limite: %.6f\n',Periodo);
         % Plot del ciclo limite evidenziato
         figure("Name", 'Ciclo Limite Evidenziato');
         plot(y, z, 'c'); 
