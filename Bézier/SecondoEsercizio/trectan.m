273c436ff0f0d04d658488b25ad8ee9829356fa3
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-22 17:56:42 +0200

    Revise documentation for trectan function
    Updated function documentation for trectan to improve clarity and consistency.

--- Bézier/SecondoEsercizio/trectan.m
+++ Bézier/SecondoEsercizio/trectan.m
@@ -1,14 +1,13 @@
 function trectan(xmi,xma,ymi,yma,color) ;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
-%%   funzione trectan(xmi,xma,ymi,yma,color) 
-%%   
-%%   Input  : xmin,xmax,ymin,ymax coordinate dei vertici
+%%   Function trectan(xmi,xma,ymi,yma,color)
 %%
-%%   Output : visualizzazione del rettangolo
+%%   Input  : xmin,xmax,ymin,ymax vertex coordinates
+%%
+%%   Output : rectangle visualization
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
 xx(1)=xmi;xx(2)=xma;
 xx(3)=xx(2);xx(4)=xx(1);xx(5)=xx(1);
 yy(1)=ymi;yy(2)=yy(1);
