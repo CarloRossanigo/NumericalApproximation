673b16a56100da2c12ac23ad2b12bd05519fa0b4
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-22 17:18:20 +0200

    Translate comments to English

--- Bézier/PrimoImpatto/Bezier3.m
+++ Bézier/PrimoImpatto/Bezier3.m
@@ -1,27 +1,27 @@
-%% Curve di Bèzier
+%% Bézier Curve
 clear all;
 close all;
-C1=1; % controllo scelta di punti: 1-> random
-C2=0; % controllo ordine punti : 1->random
-m=2; %numero di punti: m+1=3
+C1=1; % Control for point selection: 1 -> random
+C2=0; % Control for point order: 1 -> random
+m=2; % Number of points: m+1 = 3
 P0=[0,1];
 P1=[1,0];
 P2=[3,4];
 P=[P0;P1;P2];
 if(C1==1)
     P=rand(m+1,2)*10;
-   % fprintf('Punti selezionati:');
-    C2=0; %I punti sono generati random, quindi hanno già una disposizone randomica
+   % fprintf('Selected points:');
+    C2=0; % Points are randomly generated, so they already have a random order
 end
- %display(P)
+ % display(P)
 if(C2==1)
     P = P(randperm(m+1), :);
-    %fprintf('Punti selezionati:');
-    
+    %fprintf('Selected points:');
+
 end
 
 %display(P)
-n=100; %passo
+n=100; % Step size
 t=linspace(0,1,n);
 B=zeros(n,2);
 
@@ -33,18 +33,15 @@ end
 
 subplot(2,1,1)
 
-
-
-plot(B1(:,1),B1(:,2),'g','LineWidth',2); %Curva di Bèzier
-title("Curva di Bèzier");
+plot(B1(:,1),B1(:,2),'g','LineWidth',2); % Bézier curve
+title("Bézier Curve");
 xlabel('x');
 ylabel('y');
 grid on;
 
-
 subplot(2,1,2)
-plot([P(:, 1);P(1,1)], [P(:, 2);P(1,2)], 'ro-', 'MarkerFaceColor', 'r');  % Poligono di controllo
-title('Poligono di Controllo');
+plot([P(:, 1);P(1,1)], [P(:, 2);P(1,2)], 'ro-', 'MarkerFaceColor', 'r');  % Control polygon
+title('Control Polygon');
 xlabel('x');
 ylabel('y');
 grid on;
