87bc525563f98f2c485e05fc8cc53cf652740e84
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-22 17:43:45 +0200

    Update comments and variable names in cast1.m

--- Bézier/SecondoEsercizio/cast1.m
+++ Bézier/SecondoEsercizio/cast1.m
@@ -1,31 +1,31 @@
 function Q=cast1(t,P)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
-%%   funzione Q=cast1(t,P)
-%%   Inviluppo convesso di una curva di Bèzier
-%%   costruisco i punti di controllo
-%%   Primo step: t in [0.,0.5]
+%%   Function Q=cast1(t,P)
+%%   Convex hull of a Bézier curve
+%%   Build control points
+%%   Step 1: t in [0, 0.5]
 %%
-%%   Input  : t valore del parametro
-%%            P matrice punti di controllo
+%%   Input  : t parameter value
+%%            P control points matrix
 %%
-%%   Output : Q matrice punti di controllo
+%%   Output : Q control points matrix
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
-m=size(P,1)-1;  % P è la matrice 2D di controllo (XP, YP)
-XP = P(:,1)';  % Estraggo la colonna X dalla matrice P
-YP = P(:,2)';  % Estraggo la colonna Y dalla matrice P
+m=size(P,1)-1;  % P is the 2D control points matrix (XP, YP)
+XP = P(:,1)';  % Extract the X column from matrix P
+YP = P(:,2)';  % Extract the Y column from matrix P
 XPP=XP;
-YPP=YP; 
+YPP=YP;
 xx=XP;yy=YP;
 XPP(1)=xx(1);YPP(1)=yy(1);
 for kk=1:m
     xxx=xx;
     yyy=yy;
     for k=kk:m
-        xx(k+1)=(1.-t)*xxx(k)+t*xxx(k+1);
-        yy(k+1)=(1.-t)*yyy(k)+t*yyy(k+1);
+        xx(k+1)=(1-t)*xxx(k)+t*xxx(k+1);
+        yy(k+1)=(1-t)*yyy(k)+t*yyy(k+1);
     end
     XPP(kk+1)=xx(kk+1);YPP(kk+1)=yy(kk+1);
 end
