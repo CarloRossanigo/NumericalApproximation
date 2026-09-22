441cf2986d6fd5aef079b200cdd0c4de399f7a6c
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-22 17:53:47 +0200

    Translate comments to English in coox.m

--- Bézier/TerzoEsercizio/coox.m
+++ Bézier/TerzoEsercizio/coox.m
@@ -1,53 +1,53 @@
 function q = coox(t1, t2, P)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
-%%   Costruzione di un punto della superficie di Bézier 
-%%   usando l'algoritmo di Boor - Coox 
+%%   Construction of a point on the Bézier surface
+%%   using the Boor - Coox algorithm
 %%
-%%   P è una matrice di dimensioni (m x n x 3), dove ogni elemento
-%%   P(k, l, :) contiene le coordinate (x, y, z) del punto di controllo
+%%   P is a matrix of size (m x n x 3), where each element
+%%   P(k, l, :) contains the coordinates (x, y, z) of a control point
 %%
 %%   Input:
-%%       t1, t2   : parametri sulla superficie (tipicamente tra 0 e 1)
-%%       P        : matrice (m x n x 3) di punti di controllo
+%%       t1, t2   : parameters on the surface (typically between 0 and 1)
+%%       P        : matrix (m x n x 3) of control points
 %%
 %%   Output:
-%%       q        : punto [x, y, z] sulla superficie di Bézier
+%%       q        : point [x, y, z] on the Bézier surface
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Dimensioni della matrice dei punti di controllo
-np1 = size(P, 1); % Numero di righe (m)
-np2 = size(P, 2); % Numero di colonne (n)
+% Dimensions of the control point matrix
+np1 = size(P, 1); % Number of rows (m)
+np2 = size(P, 2); % Number of columns (n)
 
-% Inizializzazione dei vettori
+% Initialization of vectors
 xx1 = zeros(np1, 1);
 yy1 = zeros(np1, 1);
 zz1 = zeros(np1, 1);
 
-% Costruzione dei punti P_q1 (approssimazione lungo la direzione 2)
+% Construction of points P_q1 (approximation along direction 2)
 for k1 = 1:np1
-    % Costruzione di un vettore temporaneo per ogni riga P(k1, 1:n)
+    % Construction of a temporary vector for each row P(k1, 1:n)
     xx2 = zeros(np2, 1);
     yy2 = zeros(np2, 1);
     zz2 = zeros(np2, 1);
     
-    % Estrazione dei punti dalla matrice P
+    % Extraction of points from matrix P
     for k2 = 1:np2
-        % Punti lungo la direzione 2
+        % Points along direction 2
         xx2(k2) = P(k1, k2, 1);  % x
         yy2(k2) = P(k1, k2, 2);  % y
         zz2(k2) = P(k1, k2, 3);  % z
     end
     
-    % Calcolo del punto sulla curva di Bézier nella direzione 2
+    % Computation of the point on the Bézier curve in direction 2
     q = cast3d(t2, [xx2, yy2, zz2]);
     
-    % Memorizzazione del risultato
+    % Storage of the result
     xx1(k1) = q(1);
     yy1(k1) = q(2);
     zz1(k1) = q(3);
 end
 
-% Calcolo finale del punto sulla superficie di Bézier
+% Final computation of the point on the Bézier surface
 q = cast3d(t1, [xx1, yy1, zz1]);
