69c150b9bc1fcae5a3fc13a59cec323a404f89c7
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-22 17:55:46 +0200

    Translate comments in sbezier.m from Italian to English

--- Bézier/TerzoEsercizio/sbezier.m
+++ Bézier/TerzoEsercizio/sbezier.m
@@ -1,45 +1,45 @@
 function [TRI, Q] = sbezier(T, P)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%   Campiona la superficie di Bézier utilizzando l'algoritmo di de Boor - Coox
-%%   Input :  T valori di campionamento (matrice n x n)
-%%            P matrice (m x n x 3) dei punti di controllo
-%%   Output : TRI lista dei triangoli che compongono la superficie
-%%            Q matrice con le coordinate dei punti campionati
+%%   Samples the Bézier surface using the de Boor - Cox algorithm
+%%   Input :  T sampling values (n x n matrix)
+%%            P control points matrix (m x n x 3)
+%%   Output : TRI list of triangles composing the surface
+%%            Q matrix with the sampled point coordinates
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-n = size(T, 2);  % Numero di punti di campionamento (griglia n x n)
-n1 = n - 1;      % Numero di triangoli su una griglia (n-1) x (n-1)
-n2 = n * n;      % Numero totale di punti campionati
+n = size(T, 2);  % Number of sampling points (grid n x n)
+n1 = n - 1;      % Number of triangles on one grid (n-1) x (n-1)
+n2 = n * n;      % Total number of sampled points
 
-% Inizializzo Q per memorizzare le coordinate dei punti
+% Initialize Q to store point coordinates
 Q = zeros(n2, 3);
 
-% Calcolo i punti sulla superficie Bézier
+% Compute the points on the Bézier surface
 for k1 = 1:n
     for k2 = 1:n
-        t1 = T(k1);  % Parametro t1
-        t2 = T(k2);  % Parametro t2
-        q = coox(t1, t2, P);  % Calcolo il punto usando l'algoritmo CAGD_coox
-        k = k1 + (k2 - 1) * n;  % Indice del punto nella matrice Q
-        Q(k, :) = q;     % Memorizzo il punto in Q
+        t1 = T(k1);  % Parameter t1
+        t2 = T(k2);  % Parameter t2
+        q = coox(t1, t2, P);  % Compute the point using the CAGD_coox algorithm
+        k = k1 + (k2 - 1) * n;  % Index of the point in Q
+        Q(k, :) = q;     % Store the point in Q
     end
 end
 
-% Costruisco i triangoli (facce della superficie)
-m = n1 * n1 * 2;  % Numero di triangoli
-TRI = zeros(m, 3);  % Inizializzo la matrice TRI
+% Build the triangles (faces of the surface)
+m = n1 * n1 * 2;  % Number of triangles
+TRI = zeros(m, 3);  % Initialize TRI matrix
 
-% Creo i triangoli tra i punti adiacenti
+% Create triangles between adjacent points
 for k1 = 1:n1
     for k2 = 1:n1
         k = k1 + (k2 - 1) * n1;
-        kk = k1 + (k2 - 1) * n;  % Indice del punto corrente
-        % Primo triangolo
+        kk = k1 + (k2 - 1) * n;  % Current point index
+        % First triangle
         TRI(2 * k - 1, 1) = kk;
         TRI(2 * k - 1, 2) = kk + 1;
         TRI(2 * k - 1, 3) = kk + 1 + n;
-        % Secondo triangolo
+        % Second triangle
         TRI(2 * k, 1) = kk;
         TRI(2 * k, 2) = kk + n;
         TRI(2 * k, 3) = kk + n + 1;
