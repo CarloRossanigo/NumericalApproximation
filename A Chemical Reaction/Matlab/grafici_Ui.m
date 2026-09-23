8c3de25cba67625ad6127b60c463c3076587e5a3
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-23 14:23:33 +0200

    Refactor MATLAB script for clarity and updates

--- A Chemical Reaction/Matlab/grafici_Ui.m
+++ A Chemical Reaction/Matlab/grafici_Ui.m
@@ -2,29 +2,25 @@ clear all
 close all
 format long
 
-
-% carico il file:
-% il file .txt va copiato dalla cartella col main nella cartella di
-% matlab,
+% Load the file:
+% The .txt file must be copied from the folder with main to the MATLAB folder
 File=load('test_toll10^-14');
 
- 
-N=15483; %dipende da numero step del file ed è il numero di passi fatti da lobatto,
-                % viene stampato a compilatore ogni volta che eseguiamo il
-                % programma e va ogni volta cambiato come il file
+N=15483; % Depends on the number of steps in the file and is the number of steps performed by Lobatto,
+                % printed by the compiler each time the program is executed and must be changed along with the file
 
 t=zeros(1,N);u1=zeros(1,N);u2=zeros(1,N);u3=zeros(1,N);
-% suddivido il contenuto del File in t,u1,u2,u3
+% Split the File content into t, u1, u2, u3
 for j=1:N
-    
+
     t(j)=File(j,1);
     u1(j)=File(j,2);
     u2(j)=File(j,3);
     u3(j)=File(j,4);
-    
+
 end
 
-% adesso separo la parte di tempi minori di 10 che non mi interessa dal resto
+% Now separate the part with times less than 10 (not needed) from the rest
 i=1;
 while t(i)<10
     i=i+1;
@@ -35,27 +31,24 @@ y= u2(i-1:N);
 z= u3(i-1:N);
 w= y-z;
 
-%grafico in scala loglog
+% Log-log scale plot
 figure("Name",'log log x')
-loglog(tempo,x);% nella seconda variabile bisogna mettere x,y,z,w..
-                    
+loglog(tempo,x);% In the second variable, you must use x, y, z, w..
+
 figure("Name",'loglog y')
-loglog(tempo,y); 
+loglog(tempo,y);
 figure("Name",'loglog z')
-loglog(tempo,z); 
+loglog(tempo,z);
 figure("Name",'loglog w')
 loglog(tempo,w);
-%plot con scala semilog
-figure("Name",'semiloglog x')
+% Semi-log scale plot
+figure("Name",'semilog x')
 semilogx(tempo,x);
-%figure("Name",'semiloglog y')
+%figure("Name",'semilog y')
 %semilogx(tempo,y);
 %grid on
-%figure("Name",'semiloglog z')
+%figure("Name",'semilog z')
 %semilogx(tempo,z);
-figure("Name",'semiloglog w')
+figure("Name",'semilog w')
 semilogx(tempo,w);
 %grid on
-
-
-
