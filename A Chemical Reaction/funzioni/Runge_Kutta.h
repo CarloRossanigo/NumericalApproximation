6e72641338a56fb95440fb6db035a5f82a2c63d3
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-23 14:35:51 +0200

    Refactor Runge_Kutta.h for consistency and clarity

--- A Chemical Reaction/funzioni/Runge_Kutta.h
+++ A Chemical Reaction/funzioni/Runge_Kutta.h
@@ -1,32 +1,32 @@
 typedef  double Real;
 void step(Real *v, Real h, Real *a, Real *K, int stadi, int d, Real *y);
 
-// metodi espliciti
+// explicit methods
 void EE(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void EM(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void RK4(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void HEUN(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void RKFEHL5(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
-// metodi DIRK: diagonalmente impliciti con elementi diagonali se non nulli tutti uguali a stesso valore
+// DIRK methods: implicit diagonal with diagunal elements if not null all equals at the same value 
 void EI(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void CN(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void GAUSS1(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
-// metodi impliciti NON diagonali
+// implicit methods not diagonal 
 void GAUSS2(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void GAUSS3(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void RADAU2(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void RADAU3(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
-// metodi espliciti immersi
+// explicit immersed methods
 void RKFEHL54(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void EEHEUN(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void DP87(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
 
-// metodi impliciti DIRK immersi(adattivi)
+// implicit  DIRK immersed (adaptive) methods 
 void HAMMER(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
-// metodi impliciti NON DIAGONALI immersi (adattivi)
+// implicit NOT DIAGUNAL Immersed (adaptive) 
 void LOBATTO_3C_ORD2(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void LOBATTO_3C_ORD4(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
