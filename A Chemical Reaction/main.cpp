b78c368a1a6fa4d5fb40457fe4198d26d05c139b
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-23 14:51:46 +0200

    Update main.cpp

--- A Chemical Reaction/main.cpp
+++ A Chemical Reaction/main.cpp
@@ -9,25 +9,25 @@
 using namespace std;
 typedef double Real;
 const Real pi=4.0*atan(1.0);
-//definizione funzioni fnewton e Jfnewton per iterazioni di Newton
+//definition of the functions fnewton and Jfnewton for Newton's iterations 
 void effe_newton(Real *F,Real *X);
 void Jeffe_newton(Real *J,Real *X);
 
-// puntatori per puntare ai dati del problema
-void(*funz_effe)(Real*, Real,Real*);                           // funzione del campo vettoriale dell'equazione differenziale
-void(*dati)(Real *,Real *,Real *);                          //dati iniziali del problema
-void(*butcher)(int, Real *,Real *,Real *,Real *, int *);    //coefficienti della tabella butcher
-void(*Jeffe)(Real*,Real,Real*);                                //jacobiano di f
+// pointers to point ad problem data 
+void(*funz_effe)(Real*, Real,Real*);                           // Function of the vector field of the differential equation 
+void(*dati)(Real *,Real *,Real *);                          // Initial data of the problem
+void(*butcher)(int, Real *,Real *,Real *,Real *, int *);    // Butcher's table coefficients 
+void(*Jeffe)(Real*,Real,Real*);                                //jacobian of f
 
-// variabili comuni
+// common variables
 Real t,T,h,hnew;
-// dimensione,dati del problema e dati del metodo
-unsigned long valf;                                // variabile per il conteggio delle valutazioni di f
-const int dim=3;                                           //dimensione del problema..avendo una costante del moto non è 4 ma 3
-const Real p=4.0;                                           //ordine del metodo di ordine maggiore:nel caso di Lobatto 3c di ordine 4 l'ordine è.. 4
+// dimension, problme data e method data 
+unsigned long valf;                                // variable for counting the evalution of f 
+    const int dim=3;                                           //Dimension of the problem, with a constan of the motus is 3 not 4  
+const Real p=4.0;                                           //order of the method with the highest order : lobatto 3c has order 4
 Real u[dim];
-// imposto la tabella di butcher
-    const int s=3; // numero di stadi
+//initialize butcher's table 
+    const int s=3; // number of stadium 
     Real b[s];
     Real bc[s];
     Real c[s];
@@ -37,11 +37,11 @@ Real u[dim];
 
 int main ()
 {
-    funz_effe=eqdiff_f;                                     //funzione del campo
-    dati=dati_iniziali;                                      //dati iniziali
-    butcher=LOBATTO_3C_ORD4;                                //matrice dei coefficienti del metodo
-    Jeffe=jf;                                               //jacobiano di f
-    butcher(s,A[0],b,c,bc,0);                              // metto 0 se sono vettori che nel metodo richiamato non servono
+    funz_effe=eqdiff_f;                                     //Field function
+    dati=dati_iniziali;                                      //Initial data
+    butcher=LOBATTO_3C_ORD4;                                //matrix coeeficients of the method 
+    Jeffe=jf;                                               //jacobian of f
+    butcher(s,A[0],b,c,bc,0);                              // equals 0 of teh vectors in the methods are useless 
 
 //    test: stampo le variabili inizializzate
 //    stampamat(A[0], 3.0, 3.0,0);
@@ -50,7 +50,7 @@ int main ()
 //    stampa(3, 10.0, bc,0,0);
 
 
-    // impostiamo il file su cui stampare
+    // Set the file where to print 
     char n_file[21]= {0};
     cout << "inserire nome del file di stampa(max 20 caratteri):";
     cin >> n_file ;
@@ -67,34 +67,34 @@ int main ()
     Real toll=1e-14;                                        //tolleranza per il passo adattivo di lobatto
     Real r=0.5;
     unsigned long n=0;
-    for(int i=0; i<s; i++)                                 //con questo for calcolo la differenza b-bc che è quello che ci serve per calcolare l'errore ad ogni passo
+    for(int i=0; i<s; i++)                                 //con questo for calcolo la differenza b-bc che Ã¨ quello che ci serve per calcolare l'errore ad ogni passo
     {
         bc[i]=b[i]-bc[i];
 //        test: stampa la differenza b-bc
 //        cout << "la differenza b-bc:  " << bc[i];
     }
-    // parametri newton
+    // newton parameters
     Real toll_newton=1e-14;                                 //tolleranza per quando si approssima con newton i valori dei Ki
     int nitmax=20;
     valf=0;
 
     while(t<T)
     {
-        // Costruisco K per ogni n
+        // building K for each  n
         for(int i=0; i<s; i++)
         {
-            // iterazione Newton
+            // Newton's iterations
             int nit =0;
             newtonVERO_sist(effe_newton,Jeffe_newton, dim*s, K[0], &nit,toll_newton,nitmax);
         }
         Real err[dim], Err;
-        step(0, 1, bc, K[0], s, dim, err);                 //qui fa il prod matrice di bc con K cioè un prod vettore,poi pone err= h*[bc]*[K]
+        step(0, 1, bc, K[0], s, dim, err);                 //qui fa il prod matrice di bc con K cioÃ¨ un prod vettore,poi pone err= h*[bc]*[K]
         Err=h*norm_2(err,dim);
-        hnew=pow(r*toll/Err, 1.0/p)*h;                      //questa è la regola per aggiornare h
+        hnew=pow(r*toll/Err, 1.0/p)*h;                      //questa Ã¨ la regola per aggiornare h
         if(Err<=toll)                                       //questo if else fa il seguente:
-        {                                                   // se l'errore è meno della tolleranza stampa e aggiorna il passo al minimo fra hnew e T-t
+        {                                                   // se l'errore Ã¨ meno della tolleranza stampa e aggiorna il passo al minimo fra hnew e T-t
             step(u, h, b, K[0], s, dim, u);
-            t+=h;                                           //se l'errore è più della tolleranza aggiorna h ad hnew e il for continua
+            t+=h;                                           //se l'errore Ã¨ piÃ¹ della tolleranza aggiorna h ad hnew e il for continua
             h=min(hnew,T-t);
             n++;
             stampa(dim,t,u,0,&prt);
@@ -117,7 +117,7 @@ int main ()
 void effe_newton(Real *F,Real *KK)
 {
     Real Z[dim]={0};
-    //costruisco i k
+    //building k
     for(int i=0; i<s; i++)
     {
         step(u,h,A[i],KK,s,dim,Z);
@@ -126,7 +126,7 @@ void effe_newton(Real *F,Real *KK)
     }
     for(int i=0; i<s*dim; i++)
     {
-        F[i]=KK[i]-F[i]; // F corsivo
+        F[i]=KK[i]-F[i]; // F corsive
     }
 
 }
@@ -136,13 +136,13 @@ void Jeffe_newton(Real *J,Real *KK)
 {
     Real Jff[dim][dim];
     Real Z[dim]={0};
-    //ciclo sulle righe
+    //cicle on the row
     for (int i=0; i<s; i++)
     {
         step(u,h,A[i],KK,s,dim,Z);
         Jeffe(Jff[0],t+c[i]*h,Z);
         valf+=dim;
-        //ciclo sulle colonne
+        //cicle on the columns 
         for (int j=0; j<s; j++)
         {
             Real aij= -A[i][j]*h;
