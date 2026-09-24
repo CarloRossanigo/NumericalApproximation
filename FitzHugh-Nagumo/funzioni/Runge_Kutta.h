70e271ffcd6d80ba8ab6afd3053ee189960ac0e3
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-24 11:09:21 +0200

    Update Runge_Kutta.h

--- FitzHugh-Nagumo/funzioni/Runge_Kutta.h
+++ FitzHugh-Nagumo/funzioni/Runge_Kutta.h
@@ -1,32 +1,32 @@
 typedef  double Real;
 void step(Real *v, Real h, Real *a, Real *K, int stadi, int d, Real *y);
 using namespace std;
-// metodi espliciti
+// Explicit methods
 void EE(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void EM(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void RK4(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void HEUN(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void RKFEHL5(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
-// metodi DIRK: diagonalmente impliciti con elementi diagonali se non nulli tutti uguali a stesso valore
+// DIRK methods : Diagunally implicit with diagunal eleemnts null or with the same value 
 void EI(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void CN(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void GAUSS1(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
-// metodi impliciti NON diagonali
+// implicit methods NOT diagunal 
 void GAUSS2(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void GAUSS3(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void RADAU2(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void RADAU3(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
-// metodi espliciti immersi
+// Immersed Explicit Methods 
 void RKFEHL54(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void EEHEUN(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 void DP87(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
 
-// metodi impliciti DIRK immersi(adattivi)
+// Implicit DIRK methods (adaptive)
 void HAMMER(int ns, Real *A, Real *b, Real *c, Real *bc,int *Ind);
 
-// metodi impliciti NON DIAGONALI immersi (adattivi)
+// Implicit NOT Diagunal Immersed methods (adaptive) 
 void LOBATTO_3C_ORD2(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
 void LOBATTO_3C_ORD4(int ns, Real *A, Real *b, Real *c, Real *bc, int *Ind);
