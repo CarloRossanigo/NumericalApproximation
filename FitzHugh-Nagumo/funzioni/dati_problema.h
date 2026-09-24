9ac1682917449fa2d12cc904cc7717ae38d2bc5f
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-24 11:11:09 +0200

    Correct formatting and comments in dati_problema.h

--- FitzHugh-Nagumo/funzioni/dati_problema.h
+++ FitzHugh-Nagumo/funzioni/dati_problema.h
@@ -3,7 +3,7 @@ typedef  double Real;
 using namespace std;
 
 
-//definizione dati del problema
+//definiton problem data
 void dati_inizialiProb4(Real *t_0, Real *T, Real U_0[]);
 
 void eqdiff_fProb4(Real F[], Real t, Real U[]);
