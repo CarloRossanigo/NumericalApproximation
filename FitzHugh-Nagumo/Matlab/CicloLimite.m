fdd690fd6ab7087e45d332543c6c1a1e72167b6b
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-24 10:48:22 +0200

    Update comments and variable names in CicloLimite.m

--- FitzHugh-Nagumo/Matlab/CicloLimite.m
+++ FitzHugh-Nagumo/Matlab/CicloLimite.m
@@ -3,37 +3,37 @@ x = k(:,1); % tempo
 y = k(:,2); % V
 z = k(:,3); % W
 
-%%Se i passi sono 1000 toll a 10-3, altrimenti a 10-4 (entrambe)
+%%if step = 1000 the treshold=10-3, otheriws treshold=10-4 
 toll=1e-3; % toll 2
-%Trovo (100) indici per cui la |min(y)-y|<soglia
+% Finding 100 index where |min(y)-y|< treshold
 tempo=find(abs(min(y)-y)<toll,100);
 inizio=x(tempo(1));
 fprintf('Tempo di inizio del ciclo limite: %.6f\n',inizio);
 %
-n = size(tempo, 1);  % Numero di indici trovati
-Indici = zeros(n, 1);  % Vettore per i nuovi indici
-j = 1;  % Indice per il vettore Indici
-Indici(j) = tempo(1);  % Aggiungo il primo indice
+n = size(tempo, 1);  % Number of index found 
+Indici = zeros(n, 1);  % Vector for new index 
+j = 1;  %INdex for vector index 
+Indici(j) = tempo(1);  % Adding the first index 
 
-% Ciclo per selezionare solo indici non consecutivi
+% Cycle to find the non consecutive index 
 for i = 2:n
-    % Se la differenza tra il valore corrente e il precedente è maggiore di una soglia
-    if tempo(i) > tempo(i-1) + 1  % Aggiungo solo se la differenza è maggiore di 1
-        j = j + 1;  % Incrementa l'indice
-        Indici(j) = tempo(i);  % Aggiungo l'indice corrente
+    % If the difference between the current value and the precedent is higher than the treshold 
+    if tempo(i) > tempo(i-1) + 1  % Adding if the difference is higher than 1  
+        j = j + 1;  % Adding the index 
+        Indici(j) = tempo(i);  % Adding the current index 
     end
 end
 
-% Riduco la dimensione di Indici 
-Indici = Indici(1:j);  % Mantieni solo i valori validi
+% Reducing the dimensin of Index Vector  
+Indici = Indici(1:j);  % Maintening only the good values  
 Periodo=x(Indici(2))-x(Indici(1));
 
         fprintf('Periodicità del ciclo limite: %.6f\n',Periodo);
-        % Plot del ciclo limite evidenziato
+        % Plot limit cycle 
         figure("Name", 'Ciclo Limite Evidenziato');
         plot(y, z, 'c'); 
         hold on;
-        % Evidenzia il ciclo in rosso
+        
         plot(y(tempo(1):end), z(tempo(1):end), 'r', 'LineWidth', 2);
         grid on;
         xlabel('V');
