a8a54c4f383d27d5b9b72f10f89217f362e31171
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-24 11:11:58 +0200

    Refactor newton.cpp for improved clarity and structure

--- FitzHugh-Nagumo/funzioni/newton.cpp
+++ FitzHugh-Nagumo/funzioni/newton.cpp
@@ -54,7 +54,7 @@ void newtonVERO_sist(void(*effe)(Real*,Real*),void(*Jeffe)(Real*,Real*), int n,R
     for(int j=0;j<n;j++)
         J[i][j]=0;
       }
-    // inizio ciclo di Newton
+    // Begin Newton cycle
     Real delta1=1e200;
     Real delta2=0.0;
     *nit = 0;
